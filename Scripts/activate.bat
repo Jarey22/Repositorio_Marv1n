0a0ca741f25225d15f5ef51b3fb0437a7cf8ca9a
Jarey <juansrm02@gmail.com>
2024-11-20 22:39:05 -0500

    Cambios de funcionamiento a marvin

--- Scripts/activate.bat
+++ Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\GenX\chatbot-env
+set VIRTUAL_ENV=C:\Users\juane\Repositorio_Marv1n
 
 if not defined PROMPT set PROMPT=$P$G
 
@@ -16,7 +16,7 @@ if defined _OLD_VIRTUAL_PROMPT set PROMPT=%_OLD_VIRTUAL_PROMPT%
 if defined _OLD_VIRTUAL_PYTHONHOME set PYTHONHOME=%_OLD_VIRTUAL_PYTHONHOME%
 
 set _OLD_VIRTUAL_PROMPT=%PROMPT%
-set PROMPT=(chatbot-env) %PROMPT%
+set PROMPT=(Repositorio_Marv1n) %PROMPT%
 
 if defined PYTHONHOME set _OLD_VIRTUAL_PYTHONHOME=%PYTHONHOME%
 set PYTHONHOME=
